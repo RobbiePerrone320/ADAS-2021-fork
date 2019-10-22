b1cb6dae27b39ca6aff33f7593d6351e106897f9
adas-marist <56811568+adas-marist@users.noreply.github.com>
2019-10-22 12:09:33 -0400

    Add Environment Variables Batch File

---
+++ keys.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+ECHO setting api keys in evironment variables
+SET KEY_DARK=37293811b431cce9936ceaff098a0529
+SET KEY_OPEN=a9c1b3fb9e52b876bb573b3014cc12bd
+SET
+ECHO.
+ECHO Dark and Open Keys set
