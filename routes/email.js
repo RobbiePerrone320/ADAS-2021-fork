05ed08117e0d6217a6d22697f6753d8d37df4065
Robert <robert.liskin1@marist.edu>
2019-10-29 19:05:53 -0400

    Email responses now check to make sure there is at least one person to contact. Added some comments.

--- routes/email.js
+++ routes/email.js
@@ -1,13 +1,14 @@
 var nodemailer = require('nodemailer');
 
+//NodeMailer MailOptions object. Effectively represents the email itself.
 var mailOptions = {
     from: 'cappingtest@gmail.com',
     to: '', //Queried from the database
     subject: 'ADAS Alert',
-    text: `This message is an ADAS Alert.\n
-            Heavy rain is predicted in the next four (4) days.\n`
+    text: ""
 }
 
+//NodeMailer Transporter object. Contains information regarding authentication.
 var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -18,33 +19,36 @@ var transporter = nodemailer.createTransport({
 
 /**
  * @param {DatabaseConnection} con The MySQL datbase connection where the data is stored
- * Queries the database and sends emails to all of the emails discovered
+ * Queries the database and sends emails to all of the entries discovered
  */
 exports.sendEmail = function(con){
-    con.query("SELECT discharge, twoValves, threeValves, fourValves FROM weatherData;", function(err, result){
-        if(err) throw err;
-        else{
-            mailOptions.text += `This message is an ADAS Alert.\n\n` +
-            `Heavy rain predicted in the next 4 days.\n` +  
-            `Calculated Discharge: ${result[0].discharge}m³\n\n` + 
-            `Flood Prevent Strategies:\n` + 
-            `   - Open 2 valves for ${result[0].twoValves} hours\n` + 
-            `   - Open 3 valves for ${result[0].threeValves} hours\n` + 
-            `   - Open 4 valves for ${result[0].fourValves} hours\n\n` + 
-            `Check back to website in xx hours for updates.\n\n` +
-            `This email was automatically generated. Do not reply as this inbox is unmonitored.`;
-        }
-    });
     var selectQuery = "SELECT * FROM employee;";
     con.query(selectQuery, function(err, result){
         if(err) throw err;
-        console.log("Result: " + result);
-        for(let i = 0; i < result.length; i++){
-            mailOptions.to = result[i].email;
-            transporter.sendMail(mailOptions, function(error, info){
-                if(error) console.log(error);
-                else console.log("Email sent! " + info.response);
+        else if(result.length <= 0) console.log("There are currently no emails to contact.");
+        else {
+            console.log("Amount of emails to contact: " + result.length);
+            con.query("SELECT discharge, twoValves, threeValves, fourValves FROM weatherData;", function(err, result){
+                if(err) throw err;
+                else{
+                    mailOptions.text += `This message is an ADAS Alert.\n\n` +
+                    `Heavy rain predicted in the next 4 days.\n` +  
+                    `Calculated Discharge: ${result[0].discharge}m³\n\n` + 
+                    `Flood Prevent Strategies:\n` + 
+                    `   - Open 2 valves for ${result[0].twoValves} hours\n` + 
+                    `   - Open 3 valves for ${result[0].threeValves} hours\n` + 
+                    `   - Open 4 valves for ${result[0].fourValves} hours\n\n` + 
+                    `Check back to website in xx hours for updates.\n\n` +
+                    `This email was automatically generated. Do not reply as this inbox is unmonitored.`;
+                }
             });
+            for(let i = 0; i < result.length; i++){
+                mailOptions.to = result[i].email;
+                transporter.sendMail(mailOptions, function(error, info){
+                    if(error) console.log(error);
+                    else console.log("Email sent! " + info.response);
+                });
+            }
         }
     });
 }
